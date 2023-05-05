8d3b7f989ca6844c270640ce20ca1d8190e9c55e
Gabriel dos Santos <gabrields1910@gmail.com>
2023-05-04 22:18:14 -0300

    [Paginas e Rotas] Pagina: Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="timer">durante</label>
+          <input type="text" id="timer" />
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button>Começar</button>
+      </form>
+    </HomeContainer>
+  );
+};
